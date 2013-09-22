aca1ec7309eb02cc95305fd12dbec17aa13f1b2e
Christofer Bertonha <christoferbertonha@gmail.com>
2013-09-22 20:47:18 -0300

    Added DB

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from flask import flash, redirect, render_template
 from app import app
 from app.forms import LoginForm
 
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -27,9 +28,10 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for OpenID="%s", remember_me=%s' % (form.openid.data, form.remember_me.data))
+        msg = 'Login requested for OpenID="%s", remember_me=%s'
+        flash(msg % (form.openid.data, form.remember_me.data))
         return redirect('/index')
     return render_template('login.html',
-        title = 'Sign In',
-        form=form)
-
+        title='Sign In',
+        form=form,
+        providers=app.config['OPENID_PROVIDERS'])
